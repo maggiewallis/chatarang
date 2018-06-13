0b4978fe561a165e8866ca3f915618b4ee269b05
Maggie Wallis <msw2168@columbia.edu>
2018-06-12 21:21:28 -0400

    Made all components

--- src/Message.js
+++ src/Message.js
@@ -2,10 +2,60 @@ import React from 'react'
 
 const Message = (props) => {
   return (
-    <div className="Message">
-      {props.message.userName}: {props.message.body}
+    <div className="Message" style = {styles.message}>
+       <div
+            className="Avatar"
+            style= {styles.avatarPhoto}
+          ></div>
+          <div className="details" style = {styles.details}>
+            <div className="Metadata" style = {styles.metadata}>
+              <div className="user" style = {styles.user}>{props.message.userName}</div>
+              <div className="time" style = {styles.time}>1:10 PM</div>
+            </div>
+            <div className="body">
+              {props.message.body}
+            </div>
+          </div>
     </div>
   )
 }
 
 export default Message
+
+
+const styles = {
+
+  message: {
+    display: "flex",
+    marginTop: "1rem",
+    padding: "0 1rem",
+  },
+
+  details: {
+    flex: "1",
+    paddingLeft: "0.5rem",
+  },
+
+  metadata: {
+    display: "flex",
+    alignItems: "baseline",
+  },
+  
+  user: {
+    fontWeight: "bold",
+    marginRight: "0.5rem",
+  },
+  
+  time: {
+    color: "#999",
+    fontSize: "0.8rem",
+  },
+
+  avatarPhoto: {
+    width: "40px",
+    height: "40px",
+    backgroundImage: "url(https://api.adorable.io/avatars/285/bob@adorable.io)",
+    backgroundSize: "cover",
+    borderRadius: "20px",
+},
+}
